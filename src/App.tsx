81d9fdab18a4c09d0a574b8b8c4e18f4c3f87253
Pranav-Grandhi <pranav@grandhi.net>
2022-03-30 02:37:18 +0000

    Add Globe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+import { useRef, useEffect } from "react";
 import Confetti from "react-confetti";
 import { useStore } from "./utils/store";
 import { NUMBER_OF_GUESSES } from "./utils/constants";
 import useWindowDimensions from "./utils/useWindowSize";
-import { GuessState } from "./utils/types";
+import createGlobe from "./utils/globe";
 
 const App = () => {
   const state = useStore();
   const { width, height } = useWindowDimensions();
   let guesses = [...state.guesses];
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    let phi = 0;
+
+    const globe = createGlobe(canvasRef.current!, {
+      devicePixelRatio: 2,
+      width: width!,
+      height: width!,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+      ],
+      onRender: (state) => {},
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
 
   return (
     <>
@@ -16,8 +45,8 @@ const App = () => {
       ) : (
         <></>
       )}
-      <main className="grid grid-cols-2 min-h-screen">
-        <div className="flex flex-col justify-center items-center h-full">
+      <main className="grid grid-cols-2 min-h-screen divide-x divide-neutral-300">
+        <div className="flex flex-col justify-center items-center h-full">\
           <div className="w-1/2">
             <h1 className="mb-6 font-serif font-bold text-2xl italic">
               &quot;{state.answer.context}&quot;
@@ -42,28 +71,40 @@ const App = () => {
                   name="city"
                   className="w-full rounded-md border-gray-300 shadow-sm focus:border-neutral-300 focus:ring focus:ring-neutral-200 focus:ring-opacity-50 "
                 />
-                <button type="submit" className="mt-3 px-3 py-2 w-full text-white font-medium rounded-md bg-neutral-700">
+                <button
+                  type="submit"
+                  className="mt-3 px-3 py-2 w-full text-white font-medium rounded-md bg-neutral-700"
+                >
                   Submit
                 </button>
               </form>
               <div className="mt-6 border border-neutral-300 shadow-md rounded-xl">
                 <table className="min-w-full table-fixed">
-                  <tr>
-                    <th>City</th>
-                    <th>Distance</th>
-                  </tr>
-                  {state.guesses.map((guess) => (
+                  <thead>
                     <tr>
-                      <td>{guess.guess}</td>
-                      <td></td>
+                      <th>City</th>
+                      <th>Distance</th>
                     </tr>
-                  ))}
+                  </thead>
+                  <tbody>
+                    {state.guesses.map((guess) => (
+                      <tr>
+                        <td>{guess.guess}</td>
+                        <td></td>
+                      </tr>
+                    ))}
+                  </tbody>
                 </table>
               </div>
             </div>
           </div>
         </div>
-        <div className="h-full"></div>
+        <div className="flex flex-col justify-center items-center h-full">
+          <canvas
+            ref={canvasRef}
+            style={{ width: width! / 2, height: width! / 2 }}
+          />
+        </div>
       </main>
     </>
   );
