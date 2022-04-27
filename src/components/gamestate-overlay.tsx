c2c0124930c8d15518f620492d0fd9f66f7454c6
Pranav-Grandhi <pranav@grandhi.net>
2022-04-27 18:15:50 +0000

    refactor gamestate-overlay

--- src/components/gamestate-overlay.tsx
+++ src/components/gamestate-overlay.tsx
@@ -2,6 +2,25 @@ import Confetti from "react-confetti";
 import { useStore } from "../utils/store";
 import useWindowDimensions from "../utils/useWindowSize";
 
+function Overlay({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode | React.ReactNode[];
+}) {
+  return (
+    <div className="z-10 absolute flex justify-center items-center w-full h-full bg-black bg-opacity-75">
+      <div className="z-20 absolute max-w-sm w-full px-8 py-6 rounded-3xl bg-white">
+        <div className="mb-6 pb-6 font-semibold border-b border-neutral-300 text-center">
+          {title}
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+}
+
 export default function GameStateOverlay() {
   const state = useStore();
   const { width, height } = useWindowDimensions();
@@ -13,30 +32,16 @@ export default function GameStateOverlay() {
           {state.gameState === "won" ? (
             <>
               <Confetti width={width} height={height} />
-              <div className="z-10 absolute flex justify-center items-center w-full h-full bg-black bg-opacity-75">
-                <div className="z-20 absolute max-w-sm w-full px-8 py-6 rounded-3xl bg-white">
-                  <div className="mb-6 pb-6 font-semibold border-b border-neutral-300 text-center">
-                    😀 You Won!
-                  </div>
-                  <div>
-                    The city is:{" "}
-                    <span className="font-semibold">{state.answer.name}</span>
-                  </div>
-                </div>
-              </div>
+              <Overlay title={"😀 You Won!"}>
+                The city is:{" "}
+                <span className="font-semibold">{state.answer.name}</span>
+              </Overlay>
             </>
           ) : state.gameState === "lost" ? (
-            <div className="z-10 absolute flex justify-center items-center w-full h-full bg-black bg-opacity-75">
-              <div className="z-20 absolute max-w-sm w-full px-8 py-6 rounded-3xl bg-white">
-                <div className="mb-6 pb-6 font-semibold border-b border-neutral-300 text-center">
-                  😢 You Lost.
-                </div>
-                <div>
-                  The city is:{" "}
-                  <span className="font-semibold">{state.answer.name}</span>
-                </div>
-              </div>
-            </div>
+            <Overlay title="😢 You Lost">
+              The city is:{" "}
+              <span className="font-semibold">{state.answer.name}</span>
+            </Overlay>
           ) : (
             <></>
           )}
